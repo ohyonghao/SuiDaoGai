c5939408e565d6c2e8e3f23b12e634f01aef6ff2
David Post <david@ohyonghao.com>
2019-08-13 11:07:46 -0700

    Updates state if timeout
    When connecting takes a long time we get a timeout, we then
    continue to check until it is connected.
    
    Signed-off-by: David Post <david@ohyonghao.com>

--- Command.cpp
+++ Command.cpp
@@ -19,6 +19,8 @@ QJsonDocument Command::runCommand( QStringList& parameters ){
     process->waitForFinished();
     auto djson = QJsonDocument::fromJson(process->readAllStandardOutput());
 
+    QString json(djson.toJson().toStdString().data());
+    cout << json.toStdString() << endl;
     emit commandOutput(djson);
     return djson;
 }
@@ -37,7 +39,11 @@ void Command::connectVPN(){
 
     if( !djson["country"].isUndefined() ){
         emit connectCommandOutput(djson);
+    }else{
+        checkState();
     }
+    QString json(djson.toJson().toStdString().data());
+    cout << json.toStdString() << endl;
 }
 
 void Command::disconnectVPN(){
@@ -48,4 +54,13 @@ void Command::disconnectVPN(){
 void Command::checkState(){
     auto djson = runCommand(QStringList() << "state");
     emit stateCommandOutput(djson);
+    if( !djson["country"].isUndefined() ){
+        emit connectCommandOutput(djson);
+    }
+    if( !djson["errorMessage"].isUndefined() ){
+        checkState();
+    }
+    if( !djson["state"].isUndefined() && djson["state"] == "CONNECTING" ){
+        checkState();
+    }
 }
